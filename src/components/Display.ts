16d35c30b9839f120fc3f07a564805290711d174
Josh Sprow <jsprow@gmail.com>
2021-05-09 13:15:00 -0400

    adding display components to make output easier to read.

--- src/components/Display.ts
+++ src/components/Display.ts
@@ -1,9 +1,117 @@
-import type { ObjectFormat } from "@google/dscc";
+import type {
+  Field,
+  FieldsByConfigId,
+  InteractionsById,
+  ObjectFormat,
+  ObjectRow,
+} from "@google/dscc";
+
+const Td = (value: string) => {
+  const td = document.createElement("td");
+  td.innerText = value;
+  return td;
+};
+
+const Th = (value: string) => {
+  const th = document.createElement("th");
+  th.innerText = value;
+  return th;
+};
+
+const Tr = (values: string[], kind: "th" | "td") => {
+  const tr = document.createElement("tr");
+  for (const value of values) {
+    const c = kind === "th" ? Th(value) : Td(value);
+    tr.appendChild(c);
+  }
+  return tr;
+};
+
+const Title = (title: string) => {
+  const caption = document.createElement("caption");
+  caption.innerText = title;
+  return caption;
+};
+
+const Thead = (headers: string[]) => {
+  const thead = document.createElement("thead");
+  thead.appendChild(Tr(headers, "th"));
+  return thead;
+};
+
+const Tbody = (rows: string[][]) => {
+  const tbody = document.createElement("tbody");
+  for (const values of rows) {
+    tbody.appendChild(Tr(values, "td"));
+  }
+  return tbody;
+};
+
+const Table = (title: string, headers: string[], rows: string[][]) => {
+  const table = document.createElement("table");
+  table.appendChild(Title(title));
+  table.appendChild(Thead(headers));
+  table.appendChild(Tbody(rows));
+  return table;
+};
+
+const FieldsTable = (fieldsMap: FieldsByConfigId) => {
+  const headers: (keyof Field | "configId")[] = [
+    "configId",
+    "id",
+    "name",
+    "type",
+    "concept",
+  ];
+
+  const rows: string[][] = [];
+
+  for (const [configId, fields] of Object.entries(fieldsMap)) {
+    for (const field of fields) {
+      rows.push([configId, field.id, field.name, field.type, field.concept]);
+    }
+  }
+
+  return Table("Fields", headers, rows);
+};
+
+const DataTable = (tableRows: ObjectRow[]) => {
+  const headers = ["month", "metric"];
+  const rows: string[][] = [];
+
+  for (const tableRow of tableRows) {
+    const values: string[] = [];
+    for (const [_, [value]] of Object.entries(tableRow)) {
+      values.push(value.toString());
+    }
+    rows.push(values);
+  }
+
+  return Table("Data", headers, rows);
+};
+
+const Interactions = (interactions: InteractionsById) => {
+  const div = document.createElement("div");
+
+  const pre = document.createElement("pre");
+  pre.innerText = JSON.stringify(interactions, null, 2);
+
+  div.appendChild(Title("Interactions"));
+  div.appendChild(pre);
+
+  return div;
+};
 
 export const Display = (data: ObjectFormat) => {
-  const pre = document.createElement("pre");
-  pre.id = "month-picker-display";
-  pre.innerText = JSON.stringify(data, null, 2);
+  const div = document.createElement("div");
+  div.id = "month-picker-display";
 
-  return pre;
+  const pre = document.createElement("pre");
+  pre.innerText = JSON.stringify(data.interactions[0], null, 2);
+
+  div.appendChild(FieldsTable(data.fields));
+  div.appendChild(DataTable(data.tables.DEFAULT));
+  div.appendChild(Interactions(data.interactions));
+
+  return div;
 };
