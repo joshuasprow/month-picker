a66548ba1cd2542c5c38f1b953fe214bc25a1fe8
Josh Sprow <jsprow@gmail.com>
2021-05-08 20:49:43 -0400

    refactoring.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
-import { ObjectFormat, objectTransform, subscribeToData } from "@google/dscc";
+import { objectTransform, subscribeToData } from "@google/dscc";
 import { LOCAL } from "./config";
 import * as local from "./local-data";
 import { MonthPicker } from "./month-picker";
 
-const drawViz = (data: ObjectFormat) => {
-  MonthPicker(data);
-};
-
 if (LOCAL) {
   // renders locally
-  drawViz(local.message);
+  MonthPicker(local.message);
 } else {
-  subscribeToData(drawViz, { transform: objectTransform });
+  subscribeToData(MonthPicker, { transform: objectTransform });
 }
