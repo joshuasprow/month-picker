3ce7d32116c307b38504408cc650faafd2afd0c1
Josh Sprow <jsprow@gmail.com>
2021-05-10 10:13:02 -0400

    displaying error message if dimension is not TEXT type.

--- src/components/MonthPicker.ts
+++ src/components/MonthPicker.ts
@@ -1,4 +1,5 @@
 import * as dscc from "@google/dscc";
+import { FieldsByConfigId, FieldType } from "@google/dscc";
 import { CONFIG_DIM_ID, CONFIG_INT_ID, LOCAL, VERSION } from "../config";
 
 const handleInteraction = ({
@@ -33,7 +34,6 @@ const Option = (key: dscc.RowEntry) => {
 };
 
 const Select = (data: dscc.ObjectFormat) => {
-  console.log(data);
   const dimensionId = data.fields[CONFIG_DIM_ID][0].id;
   const rows = data.tables.DEFAULT;
 
@@ -57,9 +57,47 @@ const Select = (data: dscc.ObjectFormat) => {
   return select;
 };
 
+const validateDimensionField = (fieldsMap: FieldsByConfigId): Error | null => {
+  const fields = fieldsMap[CONFIG_DIM_ID];
+
+  if (!fields) {
+    return new Error(`No fields with ID "${CONFIG_DIM_ID}`);
+  }
+  if (fields.length === 0) {
+    return new Error(`"${CONFIG_DIM_ID}" fields have 0 elements`);
+  }
+
+  const [field] = fields;
+
+  if (field.type !== FieldType.TEXT) {
+    return new Error(
+      `Dimension field is of type "${field.type}".\n` +
+        `It must be"${FieldType.TEXT}" to work correctly.`
+    );
+  }
+
+  return null;
+};
+
+const ErrorMessage = (error: Error) => {
+  console.error(error);
+
+  const span = document.createElement("span");
+  span.id = "month-picker-error";
+  span.innerText = error.message;
+
+  return span;
+};
+
 export const MonthPicker = (data: dscc.ObjectFormat) => {
   const div = document.createElement("div");
   div.id = "month-picker";
+
+  const error = validateDimensionField(data.fields);
+  if (error) {
+    div.appendChild(ErrorMessage(error));
+    return div;
+  }
 
   const span = document.createElement("span");
   span.id = "month-picker-version";
